c77974ab865cb4b84e383fb64f8b07ae692dcba1
Adel Zaripov <zaripovar1998@gmail.com>
2022-09-11 13:51:41 +0300

    fix icon output

--- src/img/icons.ts
+++ src/img/icons.ts
@@ -1,7 +1,7 @@
-import arrowBack from './arrow-back.svg';
-import clip from './clip.svg';
-import chat_settings from './chat_settings.svg';
-import send from './send.svg';
+import send from 'bundle-text:./send.svg';
+import arrowBack from 'bundle-text:./arrow-back.svg';
+import clip from 'bundle-text:./clip.svg';
+import chat_settings from 'bundle-text:./chat_settings.svg';
 
 export {
   arrowBack,
